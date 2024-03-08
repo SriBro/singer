94a2d9456dbfa066e7bc4c783feccc39aac3c166
Srinidhi Bandi <130129272+SriBro@users.noreply.github.com>
2024-03-08 22:11:45 +0530

    Create singer.py

---
+++ singer.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("Singer Veenapani wishes you a very Happy Women's Day")
+st.header("I am a passionate Singer and Youtuber!")
+st.header("To enjoy all types of melodies, subscribe: https://www.youtube.com/@VeenapaniBandi")
